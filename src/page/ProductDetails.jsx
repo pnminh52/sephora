70b596c8f27762e7ba730e2668d24d11d1963a8c
pnminh52 <phamnhatminhsuzumoe52@gmail.com>
2025-01-28 23:48:18 +0700

    rendering product details

--- src/page/ProductDetails.jsx
+++ src/page/ProductDetails.jsx
@@ -1,14 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, {useState, useEffect} from 'react'
+import { useParams } from 'react-router-dom'
 
-const ProductDetails = ({ products }) => {
-
-
+const ProductDetails = ({products}) => {
+  const {id}=useParams()
+  const [product, setProduct] = useState(null)
+  useEffect(()=>{
+    const foundProduct = products.find((product) => product.id === id);
+    setProduct(foundProduct);
+  }, [id, products])
+  if(!product){
+    return (
+      <div>
+        <h1>Not found</h1>
+      </div>
+    )
+  } 
   return (
     <div>
-     product detail page
+      <h1>Product Details</h1>
+      <div>
+        <h2>{product.name}</h2>
+        <img src={product.thumbnail} alt="" />
+        <p>{product.content}</p>
+        <p>Price: ${product.priceAfterDiscount}</p>
+      <p>Rating: {product.rating} (Based on {product.ratingCount} reviews)</p>
+      <p>Discount: {product.discount}</p>
+      <p>Voucher: {product.voucher}</p>
+      <h3>Images</h3>
+      <div>
+        {product.images.map((image, index)=>(
+          <img key={index} src={image} alt={`Product image ${index+1}`} />
+        ))}
+      </div>
+      </div>
+      
     </div>
-  );
-};
+  )
+}
 
-export default ProductDetails;
+export default ProductDetails
