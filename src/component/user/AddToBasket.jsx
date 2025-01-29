e864291eec289107b1951c5d9a80f4a1d2f54a57
pnminh52 <phamnhatminhsuzumoe52@gmail.com>
2025-01-29 12:54:05 +0700

    ui update

--- src/component/user/AddToBasket.jsx
+++ src/component/user/AddToBasket.jsx
@@ -1,16 +1,62 @@
 import React, { useState } from "react";
 
 const AddToBasket = () => {
-    return (
-    <div className="block shadow-lg sm:hidden z-50 fixed bottom-17 bg-white w-full h-16 flex items-center">
-        <div className="relative bg-[#CF112C] h-12 hover:bg-red-700 transition ease-in-out cursor-pointer px-4 py-2 mx-4 w-full text-white flex justify-center items-center rounded-full">
-        <button className="cursor-pointer text-sm font-semibold">
-            Add to Basket
-            <p className="cursor-pointer text-xs font-normal">Get It Shipped</p>
-        </button>
+  const [quantity, setQuantity] = useState(1);
+  const [isShow, setIsShow] = useState(false);
+  const toggleDropdown = () => {
+    setIsShow(!isShow);
+  };
+
+  return (
+    <div className="block sm:hidden z-50 fixed bottom-17 bg-white w-full h-16 flex items-center sm:border-b-0">
+      <div className="relative bg-[#CF112C] hover:bg-red-600 transition ease-in-out cursor-pointer px-4 py-2 mx-4 w-full text-white flex justify-center items-center rounded-full">
+        {/* Dropdown chọn số lượng */}
+        <div className="absolute left-0 border-r px-3 h-full border-white flex justify-center items-center ">
+          <select
+            className="appearance-none  bg-transparent font-thin text-white cursor-pointer outline-none"
+            value={quantity}
+            onChange={(e) => setQuantity(Number(e.target.value))}
+          >
+            {[...Array(10)].map((_, index) => (
+              <option
+                key={index + 1}
+                value={index + 1}
+                className=" text-black text-center "
+              >
+                {index + 1}
+              </option>
+            ))}
+          </select>
+          <svg
+          onClick={toggleDropdown}
+            viewBox="0 0 1024 1024"
+            className="w-4 h-4 rotate-270 "
+            xmlns="http://www.w3.org/2000/svg"
+            fill="#fff"
+          >
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              <path
+                fill="#fff"
+                d="M609.408 149.376L277.76 489.6a32 32 0 000 44.672l331.648 340.352a29.12 29.12 0 0041.728 0 30.592 30.592 0 000-42.752L339.264 511.936l311.872-319.872a30.592 30.592 0 000-42.688 29.12 29.12 0 00-41.728 0z"
+              ></path>
+            </g>
+          </svg>
         </div>
+
+        {/* Nút Add to Basket */}
+        <button className="cursor-pointer text-xs font-semibold">
+          Add to Basket
+          <p className="cursor-pointer text-xs font-normal">Get It Shipped</p>
+        </button>
+      </div>
     </div>
-    );
+  );
 };
 
 export default AddToBasket;
--- src/page/ProductDetails.jsx
+++ src/page/ProductDetails.jsx
@@ -1,81 +1,88 @@
-import React, {useState, useEffect} from 'react'
-import { useParams } from 'react-router-dom'
-import AddToBasket from './../component/user/AddToBasket';
-import UserHeader3 from '../component/user/UserHeader3';
-const ProductDetails = ({products}) => {
-  const {id}=useParams()
-  const [product, setProduct] = useState(null)
-  useEffect(()=>{
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import AddToBasket from "./../component/user/AddToBasket";
+import UserHeader3 from "../component/user/UserHeader3";
+const ProductDetails = ({ products }) => {
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+  useEffect(() => {
     const foundProduct = products.find((product) => product.id === id);
     setProduct(foundProduct);
-  }, [id, products])
-  if(!product){
+  }, [id, products]);
+  if (!product) {
     return (
       <div>
         <h1>Not found</h1>
       </div>
-    )
-  } 
+    );
+  }
   return (
     <div>
-      <div className='hidden sm:block'>
-      <UserHeader3 />
+      <div className="hidden sm:block">
+        <UserHeader3 />
       </div>
- <div className='container mx-auto px-4'>
-    <div className='flex text-sm text-gray-600 py-2'>
-      <p className=' cursor-pointer'>{product.category}</p>
-      <p className=" before:content-['›'] before:mx-1.5 cursor-pointer">
-    {product.position}
-  </p>
-  <p className=" before:content-['›'] before:mx-1.5 cursor-pointer">
-    {product.department}
-  </p>
-    </div>
-    <div className="flex w-full justify-between bg-[#A76640] ">
-  <div className='text-white flex items-center ml-4 mr-4  w-1/2  '>
-    <div>
-    <p className='text-xs cursor-pointer font-semibold'>Try Complexion Picks at Home</p>
-    <p className='text-xs cursor-pointer font-normal'>Get a foundation and concealer sample set. <strong> SHOP NOW▸</strong></p>
-    </div>
-  </div>
-<div className='w-1/2'>
-<picture >
-    <source
-      media="(min-width: 768px)"
-      srcSet="https://www.sephora.com/contentimages/ppagebanners/2024-10-8-paid-foundation-bag-site-desktop-product-page-top-banner-img-only-1226x120-us-2063.jpg?imwidth=1226 1x, https://www.sephora.com/contentimages/ppagebanners/2024-10-8-paid-foundation-bag-site-desktop-product-page-top-banner-img-only-1226x120-us-2063.jpg?imwidth=2452 2x"
-    />
-    <source
-      media="(max-width: 767px)"
-      srcSet="https://www.sephora.com/contentimages/ppagebanners/2024-10-8-paid-foundation-bag-site-mobile-app-product-page-top-banner-img-only-262x138-us-2082.jpg?imwidth=262 1x, https://www.sephora.com/contentimages/ppagebanners/2024-10-8-paid-foundation-bag-site-mobile-app-product-page-top-banner-img-only-262x138-us-2082.jpg?imwidth=524 2x"
-    />
-    <img
-      src="https://www.sephora.com/contentimages/ppagebanners/2024-10-8-paid-foundation-bag-site-mobile-app-product-page-top-banner-img-only-262x138-us-2082.jpg?imwidth=262"
-      alt="Banner"
-      className="w-auto h-auto object-cover cursor-pointer"
-    />
-  </picture>
-</div>
-</div>
+      <div className="container mx-auto px-4">
+        <div className="flex text-sm text-gray-600 py-2 sm:py-4 ">
+          <p className=" cursor-pointer">{product.category}</p>
+          <p className=" before:content-['›'] before:mx-1.5 cursor-pointer">
+            {product.position}
+          </p>
+          <p className=" before:content-['›'] before:mx-1.5 cursor-pointer">
+            {product.department}
+          </p>
+        </div>
+        <div className="flex w-full justify-between bg-[#A76640] ">
+          <div className="text-white flex items-center ml-4 mr-4  w-1/2  ">
+            <div>
+              <p className="text-xs cursor-pointer font-semibold">
+                Try Complexion Picks at Home
+              </p>
+              <p className="text-xs cursor-pointer font-normal">
+                Get a foundation and concealer sample set.{" "}
+                <strong> SHOP NOW▸</strong>
+              </p>
+            </div>
+          </div>
+          <div className="w-1/2">
+            <picture>
+              <source
+                media="(min-width: 768px)"
+                srcSet="https://www.sephora.com/contentimages/ppagebanners/2024-10-8-paid-foundation-bag-site-desktop-product-page-top-banner-img-only-1226x120-us-2063.jpg?imwidth=1226 1x, https://www.sephora.com/contentimages/ppagebanners/2024-10-8-paid-foundation-bag-site-desktop-product-page-top-banner-img-only-1226x120-us-2063.jpg?imwidth=2452 2x"
+              />
+              <source
+                media="(max-width: 767px)"
+                srcSet="https://www.sephora.com/contentimages/ppagebanners/2024-10-8-paid-foundation-bag-site-mobile-app-product-page-top-banner-img-only-262x138-us-2082.jpg?imwidth=262 1x, https://www.sephora.com/contentimages/ppagebanners/2024-10-8-paid-foundation-bag-site-mobile-app-product-page-top-banner-img-only-262x138-us-2082.jpg?imwidth=524 2x"
+              />
+              <img
+                src="https://www.sephora.com/contentimages/ppagebanners/2024-10-8-paid-foundation-bag-site-mobile-app-product-page-top-banner-img-only-262x138-us-2082.jpg?imwidth=262"
+                alt="Banner"
+                className="w-auto h-auto object-cover cursor-pointer"
+              />
+            </picture>
+          </div>
+        </div>
 
-      <div>
-        <h2>{product.name}</h2>
-        <img src={product.thumbnail} alt="" />
-        <p>{product.content}</p>
-        <p>Price: ${product.priceAfterDiscount}</p>
-      <p>Rating: {product.rating} (Based on {product.ratingCount} reviews)</p>
-      <p>Discount: {product.discount}</p>
-      <p>Voucher: {product.voucher}</p>
-      <h3>Images</h3>
-      <div>
-        {product.images.map((image, index)=>(
-          <img key={index} src={image} alt={`Product image ${index+1}`} />
-        ))}
+        <div>
+          <h2>{product.name}</h2>
+          <img src={product.thumbnail} alt="" />
+          <p>{product.content}</p>
+          <p>Price: ${product.priceAfterDiscount}</p>
+          <p>
+            Rating: {product.rating} (Based on {product.ratingCount} reviews)
+          </p>
+          <p>Discount: {product.discount}</p>
+          <p>Voucher: {product.voucher}</p>
+          <h3>Images</h3>
+          <div>
+            {product.images.map((image, index) => (
+              <img key={index} src={image} alt={`Product image ${index + 1}`} />
+            ))}
+          </div>
+        </div>
       </div>
-      </div>
- </div>
       <AddToBasket />
     </div>
-  )
-}
+  );
+};
 
-export default ProductDetails
+export default ProductDetails;
