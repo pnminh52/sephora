a04a3bde68e7be9245c4cf5004ece2da925b63bb
pnminh52 <phamnhatminhsuzumoe52@gmail.com>
2025-02-04 13:08:47 +0700

    slider logic

--- src/component/user/ChosenForYou.jsx
+++ src/component/user/ChosenForYou.jsx
@@ -1,83 +1,116 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import RatingStars from "./RatingStar";
+
 const ChosenForYou = ({ products }) => {
   const navigate = useNavigate();
+  const [startIndex, setStartIndex] = useState(0);
 
   const handleProductClick = (id) => {
     navigate(`/products/${id}`);
   };
+const handleNext = () => {
+  if (startIndex + 1 < products.length) {
+    setStartIndex(startIndex + 1);  
+  }
+};
+
+const handlePrev = () => {
+  if (startIndex - 1 >= 0) {
+    setStartIndex(startIndex - 1);  
+  }
+};
+
+
+  const visibleProducts = products.slice(startIndex, startIndex + 6);
 
   return (
-    <div
-      className="overflow-x-auto"
-      style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}
-    >
-      <div className="grid grid-cols-6 gap-3 min-w-[1200px] px-4 md:px-6 lg:px-16 py-2  mx-auto">
-        {products.length > 0 ? (
-          products.map((product) => (
-            <div
-              className="group p-3 shadow-[0px_0px_2.5px_2.5px_rgba(0,0,0,0.1)] rounded-sm cursor-pointer transition duration-300 transform hover:translate-y-[-5px]"
-              key={product.id}
-              onClick={() => handleProductClick(product.id)}
-            >
-              <div className="relative ">
-                <img
-                  className="w-full h-auto object-cover rounded"
-                  src={product.thumbnail}
-                  alt={product.name}
-                />
-                <div className="absolute  bottom-2 left-1/2 -translate-x-1/2 bg-[#404040]  ease-in-out hover:bg-[#757575] text-white text-sm w-full py-1 rounded-sm text-center font-semibold opacity-0 transition-opacity duration-300 group-hover:opacity-90">
-                  <p>Quicklook</p>
+    <div className="relative">
+      <div className="overflow-hidden">
+        <div className="grid grid-cols-6 gap-3 px-4 md:px-6 lg:px-16 py-2 mx-auto min-w-[1200px]">
+          {visibleProducts.length > 0 ? (
+            visibleProducts.map((product) => (
+              <div
+                className="group p-3 shadow-[0px_0px_2.5px_2.5px_rgba(0,0,0,0.1)] rounded-sm cursor-pointer transition duration-300 transform hover:translate-y-[-5px]"
+                key={product.id}
+                onClick={() => handleProductClick(product.id)}
+              >
+                <div className="relative">
+                  <img
+                    className="w-full h-auto object-cover rounded"
+                    src={product.thumbnail}
+                    alt={product.name}
+                  />
+                  <div className="absolute bottom-2 left-1/2 -translate-x-1/2 bg-black ease-in-out hover:bg-[#757575] text-white text-sm w-full py-1 rounded-sm text-center font-semibold opacity-0 transition-opacity duration-300 group-hover:opacity-70">
+                    <p>Quicklook</p>
+                  </div>
+                </div>
+
+                <div>
+                  {product.status && (
+                    <p className="absolute top-1 left-1 text-[10px] uppercase bg-black text-white rounded-sm px-2 font-semibold">
+                      {product.status}
+                    </p>
+                  )}
+
+                  <p
+                    className={`absolute left-1 text-[10px] uppercase bg-black text-white rounded-sm px-2 font-semibold ${
+                      product.status ? "top-[22px]" : "top-1"
+                    }`}
+                  >
+                    {product.productLine}
+                  </p>
+                </div>
+
+                <h2 className="text-sm font-semibold mt-2">{product.name}</h2>
+                <p className="text-black text-sm line-clamp-2">
+                  {product.content}
+                </p>
+                <p className="text-black text-md py-1 font-bold">
+                  {product.priceAfterDiscount}.00$
+                  {product.maxPriceAfterDiscount &&
+                    ` - ${product.maxPriceAfterDiscount}.00$`}
+                </p>
+
+                <div className="flex items-center gap-2">
+                  <p>
+                    <RatingStars rating={product.rating} />
+                  </p>
+                  <p className="text-sm">
+                    {product.ratingCount >= 1000
+                      ? (product.ratingCount / 1000).toFixed(
+                          product.ratingCount % 1000 === 0 ? 0 : 1
+                        ) + "k"
+                      : product.ratingCount}
+                  </p>
                 </div>
               </div>
-
-              <div>
-                {product.status && (
-                  <p className="absolute top-1 left-1 text-[10px] uppercase bg-black text-white rounded-sm px-2 font-semibold">
-                    {product.status}
-                  </p>
-                )}
-
-                <p
-                  className={`absolute left-1 text-[10px] uppercase bg-black text-white rounded-sm px-2 font-semibold ${
-                    product.status ? "top-[22px]" : "top-1"
-                  }`}
-                >
-                  {product.productLine}
-                </p>
-              </div>
-
-              <h2 className="text-sm font-semibold mt-2">{product.name}</h2>
-              <p className="text-black  text-sm line-clamp-2 ">
-                {product.content}
-              </p>
-              <p className="text-black text-md py-1 font-bold">
-                {product.priceAfterDiscount}.00$
-                {product.maxPriceAfterDiscount &&
-                  ` - ${product.maxPriceAfterDiscount}.00$`}
-              </p>
-
-              <div className="flex  items-center gap-2 ">
-                <p>
-                  <RatingStars rating={product.rating} />
-                </p>
-                <p className="text-sm">
-                  {product.ratingCount >= 1000
-                    ? (product.ratingCount / 1000).toFixed(
-                        product.ratingCount % 1000 === 0 ? 0 : 1
-                      ) + "k"
-                    : product.ratingCount}
-                </p>
-              </div>
-            </div>
-          ))
-        ) : (
-          <p className="col-span-full text-center text-gray-500">
-            No products available.
-          </p>
-        )}
+            ))
+          ) : (
+            <p className="col-span-full text-center text-gray-500">
+              No products available.
+            </p>
+          )}
+        </div>
       </div>
+
+      {startIndex > 0 && (
+        <button
+          className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-black text-white p-2 rounded-full shadow-lg hover:bg-gray-800"
+          onClick={handlePrev}
+        >
+          &#10094;
+        </button>
+      )}
+
+      {startIndex + 6 < products.length && (
+        <button
+          className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-black text-white p-2 rounded-full shadow-lg hover:bg-gray-800"
+          onClick={handleNext}
+        >
+          &#10095;
+        </button>
+      )}
     </div>
   );
 };
--- src/page/Homepage.jsx
+++ src/page/Homepage.jsx
@@ -2,17 +2,23 @@ import React from "react";
 import UserHeader3 from "./../component/user/UserHeader3";
 import SliderImage from './../component/user/SliderImage';
 import ChosenForYou from '../component/user/ChosenForYou';
+import NewArrivals from "../component/user/NewArrivals";
 
 const Homepage = ({products }) => {
   return (
     <div className="h-800">
       <UserHeader3 />
       <SliderImage />
-      <div className=" mt-6 mb-4 bg-white flex justify-between max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className=" mt-6 mb-2 bg-white flex  max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
         <h1 className="font-semibold text-xl sm:text-md">Chosen For You</h1>
-        <a href="" className="text-blue-600 hover:underline">Show more</a>
       </div>
       <ChosenForYou products={products} />
+      <div className=" mt-6 mb-2 bg-white flex justify-between max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+        <h1 className="font-semibold text-xl sm:text-md">New Arrivals</h1>
+        <a href="" className="text-blue-600 hover:underline">Show more</a>
+      </div>
+      <NewArrivals products={products} />
+
     </div>
   );
 };
